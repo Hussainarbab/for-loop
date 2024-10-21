021fb5e83cfd42004a4c01a97b379aeb001abd56
Hussainarbab <arbabhussan63@gmail.com>
2024-10-21 20:56:25 +0500

    commit change

--- script.js
+++ script.js
@@ -19,16 +19,40 @@
 
 
 
-const users = [
-  { firstName: "Hussain", lastName: "Abbas" },
-  { firstName: "John", lastName: "Doe" },
-  { firstName: "Jane", lastName: "Smith" }
-];
+// const users = [
+//   { firstName: "Hussain", lastName: "Abbas" },
+//   { firstName: "John", lastName: "Doe" },
+//   { firstName: "Jane", lastName: "Smith" }
+// ];
 
-const fullNames = users.map(function(user) {
-  return user.firstName + " " + user.lastName;
-});
+// const fullNames = users.map(function(user) {
+//   return user.firstName + " " + user.lastName;
+// });
 
-console.log(fullNames);  // Output: ["Hussain Abbas", "John Doe", "Jane Smith"]
+// console.log(fullNames);  
+
+
+
+
+
+// hello =() =>{
+//   alert("hussain")
+// }
+
+
+
+
+let car ={
+  name: "toyota",
+  model: "corolla",
+  year: "2024"
+
+};
+console.log(car);
+
+
+delete car["year"];
+console.log(car)
+
 
 
