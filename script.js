7d734d4836fca9231e9c65ff8ff4fa6c0b05235d
Hussainarbab <arbabhussan63@gmail.com>
2024-10-21 21:32:04 +0500

    clock commit

--- script.js
+++ script.js
@@ -1,92 +1,21 @@
-// for (let i = 100; i >= 1; i--) {
-//     console.log(i);
-// }
+
+function updateClock() {
+  const now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
 
 
+  hours = hours < 10 ? '0' + hours : hours;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
 
 
+  const currentTime = `${hours}:${minutes}:${seconds}`;
+  document.getElementById('clock').textContent = currentTime;
+}
 
 
+setInterval(updateClock, 1000);
 
-
-
-// let count = 1;
-
-// while (count <= 100) {
-//   console.log(count);
-//   count++;
-// }
-
-
-
-// const users = [
-//   { firstName: "Hussain", lastName: "Abbas" },
-//   { firstName: "John", lastName: "Doe" },
-//   { firstName: "Jane", lastName: "Smith" }
-// ];
-
-// const fullNames = users.map(function(user) {
-//   return user.firstName + " " + user.lastName;
-// });
-
-// console.log(fullNames);  
-
-
-
-
-
-// hello =() =>{
-//   alert("hussain")
-// }
-
-
-
-
-// let car ={
-//   name: "toyota",
-//   model: "corolla",
-//   year: "2024"
-
-// };
-// console.log(car);
-
-
-// delete car["year"];
-// console.log(car)
-
-
-let fruits =["banana", "mangoe", "orange","apple",]
-console.log(fruits)
-
-
-fruits.push("grapes");
-
-console.log(fruits)
-
-fruits.pop( );
-console.log(fruits)
-
-
-
-fruits.shift()
-console.log(fruits)
-
-fruits.unshift("grapes")
-console.log(fruits)
-
-
-let scndfruits =fruits.slice(1,3)
-console.log(scndfruits)
-
-
-let thrdfruits =fruits.splice(2,0, "lemon","arbab")
-console.log(thrdfruits)
-
-
-const fruitss = ["Banana", "Orange", "Apple", "Mango"];
-
-// At position 2, add "Lemon" and "Kiwi":
-fruitss.splice(2, 0, "Lemon", "Kiwi");
-
-console.log(fruitss)
-
+updateClock();
