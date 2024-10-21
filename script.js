802a8c9abaa19820ae8b0a347282f978d0d7247b
Hussainarbab <arbabhussan63@gmail.com>
2024-10-21 21:12:21 +0500

    h

--- script.js
+++ script.js
@@ -42,17 +42,51 @@
 
 
 
-let car ={
-  name: "toyota",
-  model: "corolla",
-  year: "2024"
+// let car ={
+//   name: "toyota",
+//   model: "corolla",
+//   year: "2024"
 
-};
-console.log(car);
-
-
-delete car["year"];
-console.log(car)
+// };
+// console.log(car);
 
 
+// delete car["year"];
+// console.log(car)
+
+
+let fruits =["banana", "mangoe", "orange","apple",]
+console.log(fruits)
+
+
+fruits.push("grapes");
+
+console.log(fruits)
+
+fruits.pop( );
+console.log(fruits)
+
+
+
+fruits.shift()
+console.log(fruits)
+
+fruits.unshift("grapes")
+console.log(fruits)
+
+
+let scndfruits =fruits.slice(1,3)
+console.log(scndfruits)
+
+
+let thrdfruits =fruits.splice(2,0, "lemon","arbab")
+console.log(thrdfruits)
+
+
+const fruitss = ["Banana", "Orange", "Apple", "Mango"];
+
+// At position 2, add "Lemon" and "Kiwi":
+fruitss.splice(2, 0, "Lemon", "Kiwi");
+
+console.log(fruitss)
 
