8a951ddce5686997c3898a53699929e21f487464
Hussainarbab <arbabhussan63@gmail.com>
2024-10-21 18:13:37 +0500

    commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-for (let i = 100; i >= 1; i--) {
-    console.log(i);
-}
+// for (let i = 100; i >= 1; i--) {
+//     console.log(i);
+// }
 
 
 
@@ -10,9 +10,25 @@ for (let i = 100; i >= 1; i--) {
 
 
 
-let count = 1;
+// let count = 1;
 
-while (count <= 100) {
-  console.log(count);
-  count++;
-}
+// while (count <= 100) {
+//   console.log(count);
+//   count++;
+// }
+
+
+
+const users = [
+  { firstName: "Hussain", lastName: "Abbas" },
+  { firstName: "John", lastName: "Doe" },
+  { firstName: "Jane", lastName: "Smith" }
+];
+
+const fullNames = users.map(function(user) {
+  return user.firstName + " " + user.lastName;
+});
+
+console.log(fullNames);  // Output: ["Hussain Abbas", "John Doe", "Jane Smith"]
+
+
